1e124344b300acad399e8bc6c8fb4b5e3d31276b
Brynn Smith <brynn@genesisbox.ca>
2022-06-24 02:10:32 -0400

    Create thevowelcode.js

---
+++ 6kyu/thevowelcode.js
@@ -0,0 +1,31 @@
+/* Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels. */
+
+function encode(string) {
+    const vowels = "aeiou".split('')
+    return string.split('').map(el => vowels.indexOf(el) + 1 || el).join('')
+  }
+  
+  function decode(string) {
+    const vowels = "aeiou"
+    return string.split('').map(el => {
+      if (Number(el) + 1 - 1 == el) {
+        return el = vowels.charAt(Number(el) - 1)
+      } else {
+        return el
+      }
+    }).join('')
+  }
